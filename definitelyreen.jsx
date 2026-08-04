1fe517d9d72a6df9c4f15dc7879397a1ebccbe53
MAFMZAB <mafmzab@gmail.com>
2026-08-05 00:49:12 +0800

    Create definitelyreen.jsx

---
+++ definitelyreen.jsx
@@ -0,0 +1,27 @@
+const DEFINITELYREEN = () => {
+
+  return (
+    <>
+      <p>
+        This is Reen's story.
+      </p>
+
+      <h2>
+        Some stuff
+      </h2>
+
+      <p>
+        Arsikk is probably going to complain
+        that this is obviously an AI template.
+      </p>
+    </>
+  );
+
+};
+
+
+window.friendStories =
+  window.friendStories || {};
+
+window.friendStories.definitelyreen =
+  DEFINITELYREEN;
