bcd365c26a95ba0180987b15eadacf826ca2740f
MAFMZAB <mafmzab@gmail.com>
2026-08-09 01:20:54 +0800

    Update mafmzab.jsx

--- mafmzab.jsx
+++ mafmzab.jsx
@@ -14,11 +14,42 @@ const MAFMZAB = () => {
         Arsikk is probably going to complain
         that this is obviously an AI template.
       </p>
+
+      <Message
+        messages={[
+          {
+            id: 1,
+            sender: "M.A.F.",
+            timestamp: "Right now",
+            content: [
+              {
+                parts: [
+                  {
+                    text: "This message is being rendered by the Mimo Message component."
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            id: 2,
+            sender: "MrArsikk",
+            content: [
+              {
+                parts: [
+                  {
+                    text: "WHAT"
+                  }
+                ]
+              }
+            ]
+          }
+        ]}
+      />
     </>
   );
 
 };
-
 
 window.friendStories =
   window.friendStories || {};
